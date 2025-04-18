eb651b0d4a7266f46e5d842b0b4c4fa03eba5267
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-04-18 15:27:12 +0200

    Update

--- obj/Debug/net9.0/Labb2Threads.AssemblyInfo.cs
+++ obj/Debug/net9.0/Labb2Threads.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb2Threads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70e85fddaa0eaa73e192242daf94ea2078631a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb2Threads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb2Threads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
